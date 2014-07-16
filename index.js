fbc3e5c26e8bf9bc3ceb0e0212aa86f8a087482e
dieterbeelaert <dieterbeelaert3@msn.com>
2014-07-16 19:09:42 +0200

    other strategy for serialport

--- index.js
+++ index.js
@@ -3,31 +3,46 @@
  */
 var express = require('express');
 var server = new express();
+var SerialPort = require('serialport').SerialPort;
 var DMXHandler = require("./modules/DMXHandler.js");
-var handler = new DMXHandler();
 var Context = require('./models/Context.js');
 var bodyParser = require('body-parser');
 server.use(bodyParser.urlencoded());
 server.use(bodyParser.json())
 //serve static files
 server.use("/public", express.static(__dirname + '/public'));
-server.listen(8080);
 
 
-server.get("/",function(req,res){
-   res.render('../views/index.ejs');
+var serialPort = new SerialPort('/dev/ttyACM0',{ baudrate: 9600,dataBits: 8,parity: 'none',stopBits: 1,flowControl: false});
+serialPort.on("open", function () {
+    console.log('serial port is open');
+    var handler = new DMXHandler(serialPort);
+    server.listen(8080);
+
+
+    server.get("/",function(req,res){
+        res.render('../views/index.ejs');
+    });
+
+    server.get("/console",function(req,res){
+        res.render('../views/console.ejs');
+    });
+
+    server.post('/console/cmd',function(req,res){
+        var ctx = new Context(req);
+        //channel, value
+        console.log(ctx);
+        new DMXHandler().sendValue(ctx.getParam('channel'),ctx.getParam('value'));
+        res.end();
+    });
+
 });
 
-server.get("/console",function(req,res){
-   res.render('../views/console.ejs');
-});
 
-server.post('/console/cmd',function(req,res){
-    var ctx = new Context(req);
-    //channel, value
-    console.log(ctx);
-    new DMXHandler().sendValue(ctx.getParam('channel'),ctx.getParam('value'));
-    res.end();
-});
+
+
+
+
+
 
 
--- modules/DMXHandler.js
+++ modules/DMXHandler.js
@@ -1,24 +1,20 @@
 /**
  * Created by Dieter Beelaert on 16/07/2014.
  */
-var SerialPort = require('serialport').SerialPort;
+//var SerialPort = require('serialport').SerialPort;
 
 
 
-function DMXHandler(){
-    this.serialPort = null;
-    this.isOpen = false;
-    this.serialPort = new SerialPort('/dev/ttyACM0',{ baudrate: 9600,dataBits: 8,parity: 'none',stopBits: 1,flowControl: false});
-    this.serialPort.on("open", function () {console.log('serial port is open');isOpen = true;} );
+function DMXHandler(serialport){
+    this.serialPort = serialport;
 }
 
 module.exports = DMXHandler;
 
 DMXHandler.prototype.sendValue = function(channel,value){
     console.log('called to write to serialport serialport isopen: ' + this.isOpen);
-    //if(this.isOpen) {
-    this.serialPort = new SerialPort('/dev/ttyACM0',{ baudrate: 9600,dataBits: 8,parity: 'none',stopBits: 1,flowControl: false});
-    this.serialPort.on("open", function () {console.log('serial port is open'); this.serialPort.write(channel + 'c' + value + 'w')} );
-    //}
+    if(this.isOpen) {
+     this.serialPort.write(channel + 'c' + value + 'w');
+    }
 }
 
