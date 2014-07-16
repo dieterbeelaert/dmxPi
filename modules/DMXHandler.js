dc020172397ec1849355ca971ba8409ae3816af5
dieterbeelaert <dieterbeelaert3@msn.com>
2014-07-16 18:53:10 +0200

    debug messages

--- modules/DMXHandler.js
+++ modules/DMXHandler.js
@@ -8,17 +8,16 @@ var SerialPort = require('serialport').SerialPort;
 function DMXHandler(){
     this.serialPort = null;
     this.isOpen = false;
-    //serialPort = new SerialPort('/dev/ttyACM0',{ baudrate: 9600,dataBits: 8,parity: 'none',stopBits: 1,flowControl: false});
-    //serialPort.on("open", function () {console.log('serial port is open');isOpen = true;} );
+    //this.serialPort = new SerialPort('/dev/ttyACM0',{ baudrate: 9600,dataBits: 8,parity: 'none',stopBits: 1,flowControl: false});
+    //this.serialPort.on("open", function () {console.log('serial port is open');isOpen = true;} );
 }
 
 module.exports = DMXHandler;
 
 DMXHandler.prototype.sendValue = function(channel,value){
     console.log('called to write to serialport serialport isopen: ' + this.isOpen);
-    if(this.isOpen) {
-        console.log('writing to serial port');
-        serialPort.write(channel + 'c' + value + 'w');
-    }
+    //if(this.isOpen) {
+        this.serialPort.write(channel + 'c' + value + 'w');
+    //}
 }
 
