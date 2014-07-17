0549e073b6aff15124f0c3d8ed4067791c7e5c00
dieterbeelaert <dieterbeelaert3@msn.com>
2014-07-17 11:11:02 +0200

    code refactoring

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 var express = require('express');
 var server = new express();
 var SerialPort = require('serialport').SerialPort;
-var DMXHandler = require("./modules/DMXHandler.js");
 var Context = require('./models/Context.js');
 var bodyParser = require('body-parser');
 server.use(bodyParser.urlencoded());
@@ -12,14 +11,17 @@ server.use(bodyParser.json())
 //serve static files
 server.use("/public", express.static(__dirname + '/public'));
 
-
 var serialPort = new SerialPort('/dev/ttyACM0',{ baudrate: 9600,dataBits: 8,parity: 'none',stopBits: 1,flowControl: false});
 serialPort.on("open", function () {
     console.log('serial port is open');
-    var handler = new DMXHandler(serialPort);
+    //only startup the node server when the serialport is opened ...
+    initServer();
+});
+
+
+
+function initServer(){
     server.listen(8080);
-
-
     server.get("/",function(req,res){
         res.render('../views/index.ejs');
     });
@@ -30,15 +32,14 @@ serialPort.on("open", function () {
 
     server.post('/console/cmd',function(req,res){
         var ctx = new Context(req);
-	var channel = ctx.getParam('channel');
-	var value = ctx.getParam('value');
+        var channel = ctx.getParam('channel');
+        var value = ctx.getParam('value');
         //channel, value
         console.log(ctx);
         serialPort.write(channel +'c'+value+'w');
         res.end();
     });
-
-});
+}
 
 
 
