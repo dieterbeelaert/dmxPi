de4b2c62707189f2fb4f3b163f060e7d9fd1edb1
dieterbeelaert <dieterbeelaert3@msn.com>
2014-08-01 13:36:14 +0200

    removed master fader support

--- Controllers/RawDeskController.js
+++ Controllers/RawDeskController.js
@@ -34,17 +34,7 @@ RawDeskController.prototype.doRequest = function(){
         });
 
         socket.on('fade',function(data){
-            console.log(data);
-            if(parseInt(data.cue) === -1){
-                master = parseInt(data.value);
-                updateValues(self.serialPort);
-            }else{
-                //todo: use updateCue so that the master fader has an effect ...
-                new DMXHandler().sendValue(cues[data.cue],data.value,self.serialPort);
-                //updateCue(data.cue,data.value,function(){
-                // new DMXHandler().sendValue(cues[data.cue],master,self.serialPort);
-                // });
-            }
+            new DMXHandler().sendValue(cues[data.cue],data.value,self.serialPort);
         });
 
         socket.on('set',function(data){
@@ -53,29 +43,6 @@ RawDeskController.prototype.doRequest = function(){
         });
     });
     self.ctx.res.render(faderView,this.ctx);
-}
-
-RawDeskController.updateValues = function(){
-    var handler = new DMXHandler();
-    for(var i = 0; i < cues.length; i++){
-        handler.sendValue(cues[i],master,this.serialPort);
-    }
-}
-
-RawDeskController.updateCue = function(index,value,callback){
-    var cue = cues[index];
-    if(typeof cue !== undefined) {
-        var splitted = cue.split(',');
-        var toUpdate = '';
-        for (var i = 0; i < splitted.length; i++) {
-            var curCue = splitted[i].split('@');
-            toUpdate = curCue[0] + '@' + Math.round((parseInt(curCue[1]) * (value / 255)));
-        }
-        cues[i] = toUpdate;
-        console.log(toUpdate);
-        console.log(cues[i]);
-        callback();
-    }
 }
 
 
--- modules/DMXHandler.js
+++ modules/DMXHandler.js
@@ -12,13 +12,13 @@ module.exports = DMXHandler;
 
 
 DMXHandler.prototype.sendValue = function(valueString,master,serialPort){
-if(valueString !== undefined){  
+if(valueString !== undefined){
   var values = valueString.split(',');
     var toSend = '';
     for(var i = 0; i < values.length; i++){
         if(values[i] !== '' || values[i].indexOf('@') !== -1){
             var splitted = values[i].split('@');
-            var val = Math.round(parseInt(splitted[1]) * (master/255));
+            var val = Math.round(parseInt(splitted[1]) *(master/255));
             var toAppend = splitted[0] + 'c' + val + 'w';
             toSend += toAppend;
         }
