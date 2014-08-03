4e2d62abf8954b30aca1ec9f2480e422af9f2646
dieterbeelaert <dieterbeelaert3@msn.com>
2014-08-03 13:18:26 +0200

    small change to dmxhandler

--- modules/DMXHandler.js
+++ modules/DMXHandler.js
@@ -14,17 +14,14 @@ module.exports = DMXHandler;
 DMXHandler.prototype.sendValue = function(valueString,master,serialPort){
 if(valueString !== undefined){
   var values = valueString.split(',');
-    var toSend = '';
     for(var i = 0; i < values.length; i++){
         if(values[i] !== '' || values[i].indexOf('@') !== -1){
             var splitted = values[i].split('@');
             var val = Math.round(parseInt(splitted[1]) *(master/255));
-            var toAppend = splitted[0] + 'c' + val + 'w';
-            toSend += toAppend;
+            var toSend = splitted[0] + 'c' + val + 'w';
+            serialPort.write(toSend);
         }
     }
-    console.log('to Send ...' + toSend);
-    serialPort.write(toSend);
 }
 }
 
